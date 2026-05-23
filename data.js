e76355976e14f32bc81f0e2dae8554dbf9664ffe
hassan saed <hassansdw2@gmail.com>
2026-05-23 13:28:31 +0300

    a new certificate Introduction to computer science

--- data.js
+++ data.js
@@ -4,43 +4,48 @@
 
 const myCertifications = [
     {
-        title: "Angular and TypeScript",
-        file: "Certifications/AngularandTypeScript.pdf",
-        icon: "📘"
+        "title": "Angular and TypeScript",
+        "file": "Certifications/AngularandTypeScript.pdf",
+        "icon": "📘"
     },
     {
-        title: "C#",
-        file: "Certifications/C#.pdf",
-        icon: "💻"
+        "title": "C#",
+        "file": "Certifications/C#.pdf",
+        "icon": "💻"
     },
     {
-        title: "Hassan (General)",
-        file: "Certifications/Hassan.pdf",
-        icon: "📄"
+        "title": "Hassan (General)",
+        "file": "Certifications/Hassan.pdf",
+        "icon": "📄"
     },
     {
-        title: "Programming With HTML, CSS3, Javascript",
-        file: "Certifications/ProgrammingWithHTMLandCSS3,Javascript.pdf",
-        icon: "🌐"
+        "title": "Programming With HTML, CSS3, Javascript",
+        "file": "Certifications/ProgrammingWithHTMLandCSS3,Javascript.pdf",
+        "icon": "🌐"
     },
     {
-        title: "Python 2",
-        file: "Certifications/Python2.pdf",
-        icon: "🐍"
+        "title": "Python 2",
+        "file": "Certifications/Python2.pdf",
+        "icon": "🐍"
     },
     {
-        title: "Querying Data With Transact-SQL",
-        file: "Certifications/QueringDataWithTransact.pdf",
-        icon: "🗄️"
+        "title": "Querying Data With Transact-SQL",
+        "file": "Certifications/QueringDataWithTransact.pdf",
+        "icon": "🗄️"
     },
     {
-        title: "SQL DataBase",
-        file: "Certifications/SQLDataBase.pdf",
-        icon: "💾"
+        "title": "SQL DataBase",
+        "file": "Certifications/SQLDataBase.pdf",
+        "icon": "💾"
     },
     {
-        title: "Show",
-        file: "Certifications/show.pdf",
-        icon: "🏆"
+        "title": "Show",
+        "file": "Certifications/show.pdf",
+        "icon": "🏆"
+    },
+    {
+        "title": "Introduction for computer science",
+        "file": "Certifications/IntroductionToComputerScience.pdf",
+        "icon": "🎓"
     }
 ];
